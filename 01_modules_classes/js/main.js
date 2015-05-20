133d3d4db5716e6a214483dbc70096276ae01961
Patrick Hund <pahund@ebay.com>
2015-05-20 16:52:54 +0200

    Another bugfix for broken paths referencing chunk bundles
    Change-Id: I3d76dc54314465605deac874edef2bed1b8bb119

--- 01_modules_classes/js/main.js
+++ 01_modules_classes/js/main.js
@@ -1,1 +1,14 @@
-
+/**
+ * main.js
+ *
+ * Example 1: Modules and Classes
+ *
+ * @author <a href="mailto:pahund@team.mobile.de">Patrick Hund</a>
+ * @since 20 May 2015
+ */
+require([
+    "./Logger"
+], function (Logger) {
+    var logger = new Logger("#console");
+    logger.log("Hi freaks!");
+})
