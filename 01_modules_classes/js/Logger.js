1b903977553974a1a1e7ca543a5971a49d8a2ce7
Patrick Hund <pahund@ebay.com>
2015-05-20 17:43:47 +0200

    example 1, step 2 - Logger class
    Change-Id: Ie2fca5290ac07bf218737bd16a85edd21cd3116b

--- 01_modules_classes/js/Logger.js
+++ 01_modules_classes/js/Logger.js
@@ -1,32 +1,34 @@
-function Logger(selector) {
-    this.$console = $(selector);
+class Logger {
+    constructor(selector) {
+        this.$console = $(selector);
+    }
+
+    log(msg) {
+        this.handle = this._doLog(msg);
+    }
+
+    _doLog(msg) {
+        window.clearTimeout(this.handle);
+        this._removeNew();
+        this.$console.append(this._getHtml(msg));
+        return this._scrollDown();
+    }
+
+    _removeNew() {
+        this.$console.find("p").last().removeClass("new");
+    }
+
+    _scrollDown() {
+        var _this = this;
+        return window.setTimeout(function () {
+            _this.$console.scrollTop(_this.$console.prop("scrollHeight"));
+        }, 100);
+    }
+
+    _getHtml(msg) {
+        return "<p class=\"new\">" + msg + "</p>";
+    }
 }
-
-Logger.prototype.log = function (msg) {
-    this.handle = this._doLog(msg);
-};
-
-Logger.prototype._doLog = function (msg) {
-    window.clearTimeout(this.handle);
-    this._removeNew();
-    this.$console.append(this._getHtml(msg));
-    return this._scrollDown();
-};
-
-Logger.prototype._removeNew = function () {
-    this.$console.find("p").last().removeClass("new");
-};
-
-Logger.prototype._scrollDown = function () {
-    var _this = this;
-    return window.setTimeout(function () {
-        _this.$console.scrollTop(_this.$console.prop("scrollHeight"));
-    }, 100);
-};
-
-Logger.prototype._getHtml = function (msg) {
-    return "<p class=\"new\">" + msg + "</p>";
-};
 
 export default Logger;
 
