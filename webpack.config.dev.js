d08f2239d3dc2f76d03841dd134e2c79636c2947
Patrick Hund <pahund@ebay.com>
2015-05-20 17:11:43 +0200

    Extra config for dev mode
    Change-Id: Ie12bcb28e892980b76992aff9f4fcfc4e7b374f8

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,18 +3,21 @@ var webpack = require("webpack");
 module.exports = {
     entry: {
         example01: [
+            "webpack/hot/dev-server",
             "./01_modules_classes/js/main.js"
         ],
         example02: [
+            "webpack/hot/dev-server",
             "./02_promises/js/main.js"
         ],
         example03: [
+            "webpack/hot/dev-server",
             "./03_arrow_spread/js/main.js"
         ]
     },
     output: {
         path: __dirname,
-        publicPath: "../",
+        publicPath: "/",
         filename: "dist/[name].bundle.js",
         chunkFilename: "dist/[id].bundle.js",
     },
@@ -27,6 +30,9 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devtool: "#source-map"
 };
 
