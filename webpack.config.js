7db85585c1a7ff8660c4734b8be783b63d9c39e2
Patrick Hund <pahund@ebay.com>
2015-05-21 10:17:44 +0200

    switcheroo example 2 <-> example 3
    Change-Id: I34d5245e2d9f85365a14df488a383e2334d64fb2

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
             "./01_modules_classes/js/main.js"
         ],
         example02: [
-            "./02_promises/js/main.js"
+            "./02_arrow_spread/js/main.js"
         ],
         example03: [
-            "./03_arrow_spread/js/main.js"
+            "./03_promises/js/main.js"
         ]
     },
     output: {
