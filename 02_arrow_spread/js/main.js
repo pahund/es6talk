085108734eb88276055e66eee03a5a1387cc2e7b
Patrick Hund <pahund@ebay.com>
2015-05-21 10:39:16 +0200

    before state of example 2 with ugly nested for-loops
    Change-Id: I286499eb9d19ad1540ae5d5b1b20a299caaa9115

--- 02_arrow_spread/js/main.js
+++ 02_arrow_spread/js/main.js
@@ -1,1 +1,19 @@
+import Logger from "./Logger"
+const logger = new Logger("#console"),
+
+    roman = ["I", "II", "III"],
+    arabic = [1, 2, 3],
+    alphabetical = ["a", "b", "c"];
+
+function logIt(rom, arab, alpha) {
+    logger.log(rom + ". " + arab + ". " + alpha + ")");
+}
+
+for (let romanI = 0; romanI < roman.length; romanI++) {
+    for (let arabicI = 0; arabicI < arabic.length; arabicI++) {
+        for (let alphabeticalI = 0; alphabeticalI < alphabetical.length; alphabeticalI++) {
+            logIt(roman[romanI], alphabetical[alphabeticalI], arabic[arabicI]);
+        }
+    }
+}
 
