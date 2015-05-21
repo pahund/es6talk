65b3763ed117249d76a4fbed8d27148a010d76f4
Patrick Hund <pahund@ebay.com>
2015-05-21 11:34:54 +0200

    use arrow functions
    Change-Id: I82c698cef5afbb69a78bd3874b14ebf480e7c83f

--- 02_arrow_spread/js/iterate.js
+++ 02_arrow_spread/js/iterate.js
@@ -1,13 +1,5 @@
 function iterate(...items) {
-    return function (callback) {
-        return function (...prev) {
-            items.forEach(function (item) {
-                if (typeof callback === "function") {
-                    callback(...prev.concat(item));
-                }
-            });
-        };
-    };
+    return callback => (...prev) => items.forEach(item => callback(...prev.concat(item)));
 }
 
 export default iterate;
